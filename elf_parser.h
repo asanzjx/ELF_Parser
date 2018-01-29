6668bb946db3543933f5dd7e7231e5a2463fe9cf
asanzjx <junxuezeng@gmail.com>
2018-01-29 15:32:57 +0800

    using template to parser

--- elf_parser.h
+++ elf_parser.h
@@ -14,6 +14,12 @@ void elf64_header_parser(ifstream* elf_file, ELF64_DESC *elf_desc);
 void elf64_program_header_parser(ifstream* elf_file, ELF64_DESC *elf_desc);
 void elf64_section_header_parser(ifstream* elf_file, ELF64_DESC *elf_desc);
 
+template<typename ELF_DESC,typename ELF_HEADER>ELF_HEADER* elf_header_parser(ifstream* elf_file, ELF_DESC *elf_desc,ELF_HEADER* elf_header);
+
+template<typename ELF_DESC, typename SECTION_HEADER>SECTION_HEADER* section_header_parser(ifstream* elf_file, ELF_DESC *elf_desc, SECTION_HEADER* section_header);
+
+template<typename ELF_DESC, typename PROGRAM_HEADER>PROGRAM_HEADER* program_header_parser(ifstream* elf_file, ELF_DESC *elf_desc, PROGRAM_HEADER* program_header);
+
 
 void find_section_header_name(ifstream* elf_file,ELF32_DESC* elf_desc,unsigned int sh_name);
 
